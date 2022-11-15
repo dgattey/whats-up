1797f3bc6591f830270cf38510074bb4c987a41f
Dylan Gattey <dylan.gattey@gmail.com>
2022-11-14 23:24:59 -0800

    Initial Commit

---
+++ What's Up/What_s_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_s_UpApp.swift
+//  What's Up
+//
+//  Created by Dylan Gattey on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct What_s_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
